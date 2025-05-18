bc128826d4b0be5224929973847347bcc8d62f5f
balazs burian <bur.balazs@protonmail.com>
2025-05-18 16:26:16 +0300

    xs

--- api/search.js
+++ api/search.js
@@ -9,7 +9,7 @@ export default async function handler(req, res) {
   }
 
   try {
-    const redditRes = await fetch(`https://www.reddit.com/search.json?q=${encodeURIComponent(q)}`);
+    const redditRes = await fetch(`https://www.reddit.com/search/.json?q=${encodeURIComponent(q)}`);
     const data = await redditRes.json();
 
     res.status(200).json(data);
